ec45dcdfb1ab154e08a33608ab87afaf13e9b8c1
birdpot1234 <43954211+birdpot1234@users.noreply.github.com>
2020-11-21 12:35:07 +0700

    Update userRouter.js

--- src/api/user/userRouter.js
+++ src/api/user/userRouter.js
@@ -2,7 +2,7 @@ const route = require('express')['Router']();
 const { validate_token } = require('../../middleware/validate_token')
 const userController = require('./userController')
 
-route.post('/getUserProfile',
+route.get('/getUserProfile',
     validate_token(),
     userController.getUserProfile
 )
